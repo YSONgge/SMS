562a652b07534096ab162f3024e1e7bba0f3bd9c
YSONgge <1045719777@qq.com>
2016-01-22 00:51:12 +0800

    backUP
    sms,backup to load.i will user the okHttp .

--- app/src/main/java/com/example/yeye/sms/activity/SMSActivity.java
+++ app/src/main/java/com/example/yeye/sms/activity/SMSActivity.java
@@ -4,20 +4,48 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.yeye.sms.R;
+import com.example.yeye.sms.myMethod.Backup;
 
 public class SMSActivity extends AppCompatActivity {
 
-    public int userId ;
+    public Button btnSMSBackUp, btnSMSRegeneration, btnSMSContent;
+
+    public int userId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
         userId = getIntent().getIntExtra("userId", -1);
+        btnSMSBackUp = (Button) findViewById(R.id.btn_sms_backUp);
+        btnSMSRegeneration = (Button) findViewById(R.id.btn_sms_regeneration);
+        btnSMSContent = (Button) findViewById(R.id.btn_sms_content);
+
+        OnClickListener listener = new OnClickListener();
+        btnSMSBackUp.setOnClickListener(listener);
+    }
+    public class OnClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_sms_backUp:
+                    Backup.doSMSBackup(SMSActivity.this,userId);
+                    break;
+                case R.id.btn_sms_content:
+                    break;
+                case R.id.btn_sms_regeneration:
+                    break;
+            }
+        }
     }
 
-    public static void actionStart(Context context,int userId) {
+
+    public static void actionStart(Context context, int userId) {
         Intent i = new Intent(context, SMSActivity.class);
         i.putExtra("UserId", userId);
         context.startActivity(i);
---
+++ app/src/main/java/com/example/yeye/sms/myMethod/Regeneration.java
@@ -0,0 +1,13 @@
+package com.example.yeye.sms.myMethod;
+
+/**
+ * Created by yeye on 2016/1/21.
+ */
+public class Regeneration {
+    public void doSMSRegeneration(){
+
+    }
+    public void doContactsRegeneration(){
+
+    }
+}
